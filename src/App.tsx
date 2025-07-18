f46659bc9c91b41dfc113bac685cbef288f98c29
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-07-18 17:30:18 +0300

    write html for the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import arrowIcon from "./assets/arrow-icon.svg";
+
 const App = () => {
-  return <div className="bg-amber-100">test</div>;
+  return (
+    <div className="bg-amber-100">
+      <form>
+        <label htmlFor="day">day</label>
+        <input type="text" id="day" />
+        <label htmlFor="month">month</label>
+        <input type="text" id="month" />
+        <label htmlFor="year">year</label>
+        <input type="text" id="year" />
+        <button>
+          <img src={arrowIcon} />
+        </button>
+      </form>
+      <div>
+        <p>years</p>
+        <p>monts</p>
+        <p>days</p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
